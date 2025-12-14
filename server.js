69b2c0196a9b8134f26fcfe7720ef5620b4b3a50
stynkiq <stynkiqbusiness@gmail.com>
2025-12-14 11:24:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.post("/chat", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
